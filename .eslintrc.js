4ded6b121bc7b36689a5c13d24b0c47c68530e7b
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2023-12-16 13:08:13 +0530

    initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+  rules: {
+    quotes: ["error", "double"],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
 };
